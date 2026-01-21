f4d640b3ccf9f0d0708d21165707fbee19bd9a0d
Arno Bouiron <arno.bouiron@gmail.com>
2026-01-21 16:27:12 +0100

    ci.yml updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
     }
 
     signingConfigs {
+        create("release") {
         if (keystorePropertiesFile.exists()) {
-            create("release") {
                 keyAlias = keystoreProperties["keyAlias"] as String
                 keyPassword = keystoreProperties["keyPassword"] as String
                 storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
